bc3464d72855db3248db722b7bb693f7289da837
Rafael Germann <rafael.germann@gmail.com>
2025-06-30 08:15:26 -0300

    Adding file server and healthz

--- main.go
+++ main.go
@@ -6,13 +6,24 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/", (http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz", handlerHealth)
+
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+
 	log.Printf("Serving on port: %s\n", port)
 	server.ListenAndServe()
+}
+
+func handlerHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
